1b1d378c59bd118bdf23eaf3ff02940df3637d9e
mmazitov <mazitov.nikolya@gmail.com>
2024-10-29 14:42:27 +0200

    initial commit

--- dist/js/functions.js
+++ dist/js/functions.js
@@ -1,12 +1,42 @@
 document.addEventListener('DOMContentLoaded', function () {
-	initSidebarBtn();
+	initFetchProfiles();
 });
 
-function initSidebarBtn() {
-	const resizeBtn = document.querySelector('.resize');
-	const content = document.querySelector('.wrapper');
-	resizeBtn.addEventListener('click', function(e) {
-		e.preventDefault();
-		content.classList.toggle('sb-expand');
+function initFetchProfiles() {
+	fetch('../data/profiles.json') // Убедитесь, что путь к файлу корректный
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok ' + response.statusText);
+			}
+			return response.json();
+		})
+		.then(profiles => {
+			initProfiles(profiles);
+		})
+		.catch(error => console.error('Error loading profiles:', error));
+}
+
+function initProfiles(profiles) { // Добавлено profiles как аргумент
+	const profileCardsContainer = document.querySelector('.profile-cards');
+
+	profiles.forEach(profile => {
+		const card = document.createElement('div');
+		card.classList.add('profile-card');
+
+		const img = document.createElement('img');
+		img.src = profile.avatar;
+		img.alt = profile.name;
+
+		const name = document.createElement('h2');
+		name.textContent = profile.name;
+
+		const bio = document.createElement('p');
+		bio.textContent = profile.bio;
+
+		card.appendChild(img);
+		card.appendChild(name);
+		card.appendChild(bio);
+
+		profileCardsContainer.appendChild(card);
 	});
 }
